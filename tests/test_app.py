3f77803d635b112a791da4262ad8506df9481222
kafkaontheshore5997 <adrieneyuqing.zhao@mail.utoronto.ca>
2020-10-30 13:29:06 -0400

    index & db before Templates

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from project.app import app
+from pathlib import Path
+
+def test_index():
+    tester = app.test_client()
+    response = tester.get("/", content_type="html/text")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello, World!"
+
+def test_database():
+    assert Path("flaskr.db").is_file()
